80467083691ae5fb273c408d01b235df723d9ec9
Iain McNulty <iain@inulty.com>
2019-08-17 20:54:01 +0100

    Load test environment .env

--- src/shards.cr
+++ src/shards.cr
@@ -1,6 +1,10 @@
 # Load .env file before any other config or app code
 require "dotenv"
-Dotenv.load
+if Lucky::Env.test?
+  Dotenv.load({"FIXER_IO_KEY" => "asdf"})
+else
+  Dotenv.load
+end
 
 # Require your shards here
 require "avram"
