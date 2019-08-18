6a599c72f666d2cd1d46bac9e592e873a992a9b0
Iain McNulty <iain@inulty.com>
2019-08-18 12:39:00 +0100

    Add task for importing Currencies

---
+++ tasks/fetch_currencies.cr
@@ -0,0 +1,23 @@
+#require "../spec/support/boxes/**"
+
+# Add seeds here that are *required* for your app to work.
+# For example, you might need at least one admin user or you might need at least
+# one category for your blog posts for the app to work.
+#
+# Use `Db::CreateSampleSeeds` if your only want to add sample data helpful for
+# development.
+class FetchPrices < LuckyCli::Task
+  summary "Add database records required for the app to work"
+
+  def call
+    puts "Calling the Fixer.io API to create currencies in the Database"
+    response = FixerIoService.new.currencies
+
+    currencies = JSON.parse(response)["symbols"]
+
+    puts "Saving the currencies to the database"
+    SaveCurrency.import(currencies)
+
+    puts "Done!"
+  end
+end
