2cfa663a99edece0a786510ba92e2d1555860baa
Iain McNulty <iain@inulty.com>
2019-08-27 18:48:02 +0100

    Add Currency association to Rate

--- src/models/currency.cr
+++ src/models/currency.cr
@@ -1,5 +1,8 @@
 class Currency < BaseModel
   table :currencies do
+    has_many base_rates : Rate, foreign_key: :base_currency_id
+    has_many conversion_rates : Rate, foreign_key: :conversion_currency_id
+
     column code : String
     column name : String
   end
