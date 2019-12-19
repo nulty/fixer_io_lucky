5698019b56d9e6ce6bbb0378b9dfc522c84e6711
Iain McNulty <iain@inulty.com>
2019-12-19 21:52:16 +0000

    Improve rate importing

--- spec/fixer_io_service_spec.cr
+++ spec/fixer_io_service_spec.cr
@@ -11,32 +11,71 @@ describe FixerIoService do
     fixer.client.should be_a HTTP::Client
   end
 
-  it "#latest calls the fixer.io API latest" do
+  it "#latest calls the fixer.io API historical" do
     fixture = File.read("#{__DIR__}/fixtures/fixer_latest.json")
 
-    WebMock.stub(:get, "http://data.fixer.io/api/latest?access_key=asdf").to_return(body: fixture, headers: {"Host" => "data.fixer.io"}, status: 200)
+    WebMock.stub(:get, "http://data.fixer.io/api/latest?access_key=asdf&base=EUR")
+      .to_return(body: fixture, headers: {"Host" => "data.fixer.io"}, status: 200)
 
     response = FixerIoService.new.latest
 
-    parsed_response = JSON.parse(response.body)
-    parsed_response["success"].should be_true
-    parsed_response["base"].should eq "EUR"
-    parsed_response.dig("rates", "CAD").should eq 1.472846
+    parsed_response = RatesResponse.from_json(response.body)
+    parsed_response.success.should be_true
+    parsed_response.base.should eq "EUR"
+    if rates = parsed_response.rates
+      rates["CAD"].should eq 1.472846
+    end
 
     response.success?.should be_true
   end
 
- it "#currencies calls the fixer.io API symbols" do
+  it "#latest calls the fixer.io API historical" do
+    fixture = File.read("#{__DIR__}/fixtures/fixer_latest.json")
+
+    WebMock.stub(:get, "http://data.fixer.io/api/2013-10-01?access_key=asdf&base=EUR")
+      .to_return(body: fixture, headers: {"Host" => "data.fixer.io"}, status: 200)
+
+    response = FixerIoService.new.historical("2013-10-01")
+
+    parsed_response = RatesResponse.from_json(response.body)
+    parsed_response.success.should be_true
+    parsed_response.base.should eq "EUR"
+    if rates = parsed_response.rates
+      rates["CAD"].should eq 1.472846
+    end
+
+    response.success?.should be_true
+  end
+
+  it "#currencies calls the fixer.io API symbols" do
     fixture = File.read("#{__DIR__}/fixtures/fixer_symbols.json")
 
-    WebMock.stub(:get, "http://data.fixer.io/api/symbols?access_key=asdf").to_return(body: fixture, headers: {"Host" => "data.fixer.io"}, status: 200)
+    WebMock.stub(:get, "http://data.fixer.io/api/symbols?access_key=asdf")
+      .to_return(body: fixture, headers: {"Host" => "data.fixer.io"}, status: 200)
 
     response = FixerIoService.new.currencies
 
-    parsed_response = JSON.parse(response.body)
-    parsed_response["success"].should be_true
-    parsed_response.dig("symbols", "AED").should eq "United Arab Emirates Dirham"
+    parsed_response = CurrenciesResponse.from_json(response.body)
+    parsed_response.success.should be_true
+    parsed_response.symbols["AED"].should eq "United Arab Emirates Dirham"
 
     response.success?.should be_true
+  end
+
+  it "#currencies calls the fixer.io API historical with error" do
+    fixture = File.read("#{__DIR__}/fixtures/fixer_latest_error.json")
+
+    WebMock.stub(:get, "http://data.fixer.io/api/13-12-2019?access_key=asdf&base=EUR")
+      .to_return(body: fixture, headers: {"Host" => "data.fixer.io"}, status: 200)
+
+    response = FixerIoService.new.historical("13-12-2019", "EUR")
+
+    parsed_response = RatesResponse.from_json(response.body)
+    parsed_response.success.should be_false
+
+    if error = parsed_response.error
+      error.code.should eq 105
+      error.type.should eq "base_currency_access_restricted"
+    end
   end
 end
--- src/operations/save_rate.cr
+++ src/operations/save_rate.cr
@@ -3,22 +3,23 @@ class SaveRate < Rate::SaveOperation
     # creates a new rate for each date. If rate exists for the base curr and target curre
     # then no new rate is created
 
-    base_code = response.base
-    base_currency = CurrencyQuery.new.code(base_code).first
-    raise ArgumentError.new if !base_currency
-    date = response.date
-    rates = response.rates
+    if base_code = response.base
 
-    rates.each do |code, rate_val|
-      conversion_currency = CurrencyQuery.new.code(code).first
+      base_currency = CurrencyQuery.new.code(base_code).first
+      raise ArgumentError.new if !base_currency
+      if (date = response.date) && (rates = response.rates)
+        rates.each do |code, rate_val|
+          conversion_currency = CurrencyQuery.new.code(code).first
 
-      rate = RateQuery.new.base_currency_id(base_currency.id).conversion_currency_id(conversion_currency.id).date(date).first?
-      if !rate
-        SaveRate.create!(
-          conversion_currency_id: conversion_currency.id,
-          base_currency_id: base_currency.id,
-          rate: rate_val,
-          date: date)
+          rate = RateQuery.new.base_currency_id(base_currency.id).conversion_currency_id(conversion_currency.id).date(date).first?
+          if !rate
+            SaveRate.create!(
+              conversion_currency_id: conversion_currency.id,
+              base_currency_id: base_currency.id,
+              rate: rate_val,
+              date: date)
+          end
+        end
       end
     end
   end
